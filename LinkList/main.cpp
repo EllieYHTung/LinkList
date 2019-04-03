976edfebdfa71139c670182fc484fb1cd294e589
ellie-tung <ellie.tung@foxconn.com>
2019-04-03 22:28:50 +0800

    Initial Commit

---
+++ LinkList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkList
+//
+//  Created by YunHua Tung on 2019/4/3.
+//  Copyright © 2019 YunHua Tung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
